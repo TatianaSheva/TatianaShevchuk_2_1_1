a2b90abb4a5bfd13c78b4b95aba1043650c5e581
Tatiana <t_shevchuk@list.ru>
2022-09-25 12:10:43 +0300

    Change to long

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         SalesManager salesManager = new SalesManager();
-        int[] saleStat = new int[]{1, 2, 3, 4};
+        long[] saleStat = new long[]{1, 2, 3, 4};
         salesManager.sales = saleStat;
         System.out.println(salesManager.max());
     }
